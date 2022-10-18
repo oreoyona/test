ed233bdac876d05fb62d31ffcfaae2b59524eea5
GLORE <cortesgloire@gmail.com>
2022-10-18 12:24:04 +0200

    hot fix

--- print_s.c
+++ print_s.c
@@ -15,3 +15,38 @@ void print_s(char *str)
         putchar(str[x]);
     }
 }
+
+void x_f(int l, const char *format, va_list ap)
+{
+    int x = 0;
+    char next;
+    int tmp;
+    for (x = 0; x < l; x++)
+    {
+        if (format[x] == '%')
+        {
+            next = format[x + 1];
+            if (check_format(next) == -1)
+            {
+                tmp = va_arg(ap, int);
+                putchar(format[x]);
+            }
+
+            if (check_format(next) == 0 && next == 'd')
+            {
+                tmp = va_arg(ap, int);
+                print_num(1, tmp);
+                x += 2;
+            }
+
+            if (next == 'c')
+            {
+                tmp = va_arg(ap, int);
+                putchar(tmp);
+                x += 2;
+            }
+        }
+        else
+            putchar(format[x]);
+    }
+}
